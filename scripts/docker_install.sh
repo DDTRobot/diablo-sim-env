deeba2f01f1d798219de537501626497111e69c2
rmecboy <2548222159@qq.com>
2023-11-16 19:01:27 +0800

    add nvidia-docker2 install

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -27,3 +27,15 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 sudo docker run hello-world
 
 
+
+#Install nvidia-docker2
+sudo systemctl --now enable docker
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
