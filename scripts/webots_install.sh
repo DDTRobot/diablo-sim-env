63908a5d5f3437ef7d1ed11027175158310c6c11
rmecboy <2548222159@qq.com>
2023-12-06 14:04:05 +0800

    Update webot_install sh

--- scripts/webots_install.sh
+++ scripts/webots_install.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# Install Webots and dependencies
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install --yes wget bzip2 xvfb gnuplot make&& \
-wget https://github.com/cyberbotics/webots/releases/download/R2023a/webots-R2023a-x86-64.tar.bz2 && \
-tar xjf webots-*.tar.bz2 && rm webots-*.tar.bz2
-
-# Install Webots runtime dependencies
-wget https://raw.githubusercontent.com/cyberbotics/webots/master/scripts/install/linux_runtime_dependencies.sh && \
-chmod +x linux_runtime_dependencies.sh && ./linux_runtime_dependencies.sh && rm ./linux_runtime_dependencies.sh 
-
-# Setup gdbgui and make diablo-A1 webots proj
-cd ~/diablo_A1/controllers/diablo_webots/ && make debug && \
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/gnuplot-cpp/gnuplot-cpp.zip && \
-mkdir -p /opt/gnuplot && unzip -d /opt/gnuplot gnuplot-cpp.zip && cd /opt/gnuplot/gnuplot-cpp && make &&\
-cp gnuplot_i.hpp ~/diablo_A1/controllers/diablo_webots/simulation
-  
-
+# Install Webots and 
+wget https://github.com/cyberbotics/webots/releases/download/R2023a/webots_2023a_amd64.deb
+sudo apt install ./webots_2023a_amd64.deb -y
 
 # Setup ~/.bashrc
-echo 'export TERM=xterm-256color\n\
-export PATH="$PATH:$HOME/.local/bin"\n\
-export WEBOTS_HOME=/usr/local/webots\n'\
->> ~/.bashrc
-
+echo 'export WEBOTS_HOME=/usr/local/webots' >> ~/.bashrc
 # Setup /etc/ld.so.conf
-echo '/usr/local/webots/lib/controller\n'\
->> /etc/ld.so.conf
+sudo sh -c "echo '/usr/local/webots/lib/controller' >> /etc/ld.so.conf"
 
-ldconfig
+sudo sh -c "ldconfig"
+
+# Setup gdbgui and make diablo-A1 webots proj
+cd ./diablo_A1/controllers/diablo_webots/ && make debug && \
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/gnuplot-cpp/gnuplot-cpp.zip && \
+sudo mkdir -p gnuplot && sudo unzip -d gnuplot gnuplot-cpp.zip && cd gnuplot/gnuplot-cpp && sudo make &&\
+cd ../../ && sudo cp gnuplot/gnuplot-cpp/gnuplot_i.hpp simulation/
+
+echo 'finish'
+
+
+
+
 
 
